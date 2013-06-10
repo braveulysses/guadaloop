2ba26a19a71870aaf171292a2e1246283daa4848
Jacob Childress <jacobc@gmail.com>
2013-06-09 23:55:43 -0500

    First stab at a command-line tool for Cap Metro riders
    Added a 'guadaloop' script for making location-based queries of the
    Austin Capital Metro schedule. This is in a very rough state at the
    moment.

--- lib/guadaloop.rb
+++ lib/guadaloop.rb
@@ -1,4 +1,6 @@
+require 'geocoder'
 require 'httparty'
+require 'thor'
 
 require 'guadaloop/agency'
 require 'guadaloop/client'
@@ -52,6 +54,83 @@ module Guadaloop
 
     def get_stops_near(latitude, longitude, radius=1)
       Client::request("/api/stopsNearby/#{latitude}/#{longitude}/#{radius}")
+    end
+  end
+
+  class CLI < Thor
+    no_commands {
+      class << self
+        def get_coordinates(address)
+          coords = Geocoder.search(address)
+          if coords.size < 1
+            raise Thor::Error, "Couldn't locate the given address!"
+          end
+          return coords[0].latitude, coords[0].longitude
+        end
+      end
+    }
+
+    desc "list-bus-stops ADDRESS", "Lists nearby bus stops"
+    def list_bus_stops(address)
+      latitude, longitude = CLI::get_coordinates(address)
+      stops = Guadaloop::get_stops_near(latitude, longitude, 0.5)
+      stops.each do |stop|
+        print_wrapped "#{stop['stop_id']}: #{stop['stop_name']}: #{stop['stop_desc']}"
+      end
+    end
+
+    desc "list-train-stops ADDRESS", "Lists nearby train stops"
+    def list_train_stops(address)
+      latitude, longitude = CLI::get_coordinates(address)
+      nearby_stops = Guadaloop::get_stops_near(latitude, longitude, 3)
+      train_stops = nearby_stops.select { |stop| not stop['zone_id'].empty? }
+      train_stops.each do |stop|
+        print_wrapped "#{stop['stop_id']}: #{stop['stop_name']}: #{stop['stop_desc']}"
+      end
+    end
+
+    desc "list-stops-by-route ROUTE_ID DIRECTION", "Lists stops for a route"
+    def list_stops_by_route(route_id, direction)
+      stops = Guadaloop::get_stops(route_id, direction)
+      stops.each do |stop|
+        next if stop.nil?
+        print_wrapped "#{stop['stop_id']}: #{stop['stop_name']}: #{stop['stop_desc']}"
+      end
+    end
+
+    desc "list-times ROUTE_ID STOP_ID", "Lists arrival times for a stop"
+    def list_times(route_id, stop_id)
+      times = Guadaloop::get_times(route_id, stop_id)
+      say times, :magenta
+    end
+
+    desc "list-nearby-times ADDRESS DIRECTION RADIUS", "Lists nearby routes and stops"
+    def list_nearby_times(address, direction, radius=0.5)
+      latitude, longitude = CLI::get_coordinates(address)
+      nearby_stops = Guadaloop::get_stops_near(latitude, longitude, radius)
+      nearby_routes = Guadaloop::get_routes_near(latitude, longitude, radius)
+      nearby_routes.each do |route|
+        s = Guadaloop::get_stops(route['route_id'], direction)
+        stops_for_route = s.collect { |stop| stop['stop_id'] if not stop.nil? }
+        nearby_stops.each do |nearby_stop|
+          if stops_for_route.include? nearby_stop['stop_id']
+            times = Guadaloop::get_times(route['route_id'], nearby_stop['stop_id'])
+            next if times.kind_of? Hash and times.has_key? "error"
+            say "Route: #{route['route_short_name']} #{route['route_long_name']}, Stop: #{nearby_stop['stop_name']}"
+            say times
+          end
+        end
+      end
+    end
+
+    desc "next-bus DIRECTION STOP_ID", "Displays the next bus arrival time"
+    def next_bus(direction, stop_id)
+      say "this does nothing", :green
+    end
+
+    desc "next-train DIRECTION STOP_ID", "Displays the next train arrival time"
+    def next_train(direction, stop_id)
+      say "this does nothing", :green
     end
   end
 end
